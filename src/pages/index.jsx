f53044ebf5dc7f4a1f3d17f3ab3e147207358c99
paulosilva <paulosilva@chinasystems.com>
2020-11-10 16:13:59 +0000

    Add RTL config and first test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 
-export default function Home({ title = 'React Avançado!' }) {
+export default function Home({ title = 'React Advanced!' }) {
   return (
     <div className={styles.container}>
       <Head>
