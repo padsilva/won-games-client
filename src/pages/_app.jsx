67185dfa1b0da8490daf91fab20a52b474653dff
paulosilva <paulosilva@chinasystems.com>
2020-11-20 15:42:59 +0000

    Initial commit from Create Next App

---
+++ src/pages/_app.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+const App = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Advanced React - Boilerplate</title>
+      <link rel="shortcut icon" href="/img/icon-512.png" />
+      <link rel="apple-touch-icon" href="/img/icon-192.png" />
+      <link rek="manifest" href="/manifest.json" />
+      <meta
+        name="description"
+        content="A simple project starter to work with JavaScript, React, Next.js and Styled Components"
+      />
+    </Head>
+    <GlobalStyles />
+    <Component {...pageProps} />
+  </>
+)
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired
+}
+
+export default App
