8703b499404a03b26a7e4b9cbb3aecdfd5f971c6
paulosilva <paulosilva@chinasystems.com>
2020-12-11 16:18:58 +0000

    Add Form component to sign-in and sign-up pages

--- src/pages/sign-in.jsx
+++ src/pages/sign-in.jsx
@@ -1,19 +1,10 @@
-import TextField from 'components/TextField'
 import Auth from 'templates/Auth'
+import FormSignIn from 'components/FormSignIn'
 
 const SignIn = () => (
   <Auth title="Sign In">
-    <TextField />
+    <FormSignIn />
   </Auth>
 )
 
 export default SignIn
-
-//   onInput: PropTypes.func,
-//   label: PropTypes.string,
-//   labelFor: PropTypes.string,
-//   initialValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-//   icon: PropTypes.element,
-//   iconPosition: PropTypes.oneOf(['left', 'right']),
-//   disabled: PropTypes.bool,
-//   error: PropTypes.string
--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,19 +1,10 @@
-import TextField from 'components/TextField'
 import Auth from 'templates/Auth'
+import FormSignUp from 'components/FormSignUp'
 
 const SignUp = () => (
   <Auth title="Sign Up">
-    <TextField />
+    <FormSignUp />
   </Auth>
 )
 
 export default SignUp
-
-//   onInput: PropTypes.func,
-//   label: PropTypes.string,
-//   labelFor: PropTypes.string,
-//   initialValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-//   icon: PropTypes.element,
-//   iconPosition: PropTypes.oneOf(['left', 'right']),
-//   disabled: PropTypes.bool,
-//   error: PropTypes.string
