8961f1e5234beeb8e31be83da02764af0c16a9e1
paulosilva <paulosilva@chinasystems.com>
2020-12-11 11:15:29 +0000

    Change head metadata and title

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,14 +8,11 @@ import theme from 'styles/theme'
 const App = ({ Component, pageProps }) => (
   <ThemeProvider theme={theme}>
     <Head>
-      <title>Advanced React - Boilerplate</title>
+      <title>Won Games</title>
       <link rel="shortcut icon" href="/img/icon-512.png" />
       <link rel="apple-touch-icon" href="/img/icon-192.png" />
       <link rek="manifest" href="/manifest.json" />
-      <meta
-        name="description"
-        content="A simple project starter to work with JavaScript, React, Next.js and Styled Components"
-      />
+      <meta name="description" content="The best Game Stores in the world!" />
     </Head>
     <GlobalStyles />
     <Component {...pageProps} />
