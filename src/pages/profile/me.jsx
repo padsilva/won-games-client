50f3c4cdec085255719817104aa513166f536cf3
paulosilva <paulosilva@chinasystems.com>
2021-01-04 15:06:49 +0000

    Profile page

---
+++ src/pages/profile/me.jsx
@@ -0,0 +1,10 @@
+import Profile from 'templates/Profile'
+import FormProfile from 'components/FormProfile'
+
+const Me = () => (
+  <Profile>
+    <FormProfile />
+  </Profile>
+)
+
+export default Me
