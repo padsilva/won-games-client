98d3caecfa5d121f8c6c0b74f4fc8522dcad66f9
paulosilva <paulosilva@chinasystems.com>
2021-03-01 17:04:53 +0000

    add mappers test

---
+++ src/utils/mappers/test.js
@@ -0,0 +1,101 @@
+import { bannerMapper, gamesMapper, highlightMapper } from '.'
+
+describe('bannerMapper()', () => {
+  it('sould return the right format when mapped', () => {
+    const banner = {
+      image: {
+        url: '/image.jpg'
+      },
+      title: 'Banner title',
+      subtitle: 'Banner sutitle',
+      button: {
+        label: 'button label',
+        link: 'button link'
+      },
+      ribbon: {
+        text: 'ribbon text',
+        color: 'primary',
+        size: 'normal'
+      }
+    }
+
+    expect(bannerMapper([banner])).toStrictEqual([
+      {
+        img: 'http://localhost:1337/image.jpg',
+        title: 'Banner title',
+        subtitle: 'Banner sutitle',
+        buttonLabel: 'button label',
+        buttonLink: 'button link',
+        ribbon: 'ribbon text',
+        ribbonColor: 'primary',
+        ribbonSize: 'normal'
+      }
+    ])
+  })
+})
+
+describe('gamesMapper()', () => {
+  it('sould return an empty array if there are no games', () => {
+    expect(gamesMapper(null)).toStrictEqual([])
+  })
+
+  it('sould return the right format when mapped', () => {
+    const game = {
+      id: '1',
+      name: 'game',
+      developers: [
+        {
+          name: 'developer'
+        }
+      ],
+      slug: 'game',
+      cover: {
+        url: '/image.jpg'
+      },
+      price: 10
+    }
+
+    expect(gamesMapper([game])).toStrictEqual([
+      {
+        id: '1',
+        slug: 'game',
+        title: 'game',
+        developer: 'developer',
+        img: 'http://localhost:1337/image.jpg',
+        price: 10
+      }
+    ])
+  })
+})
+
+describe('highlightMapper()', () => {
+  it('sould return an empty object if there are no highlights', () => {
+    expect(highlightMapper(null)).toStrictEqual({})
+  })
+
+  it('sould return the right format when mapped', () => {
+    const highlight = {
+      title: 'title',
+      subtitle: 'subtitle',
+      background: {
+        url: '/image.jpg'
+      },
+      buttonLabel: 'button label',
+      buttonLink: 'button link',
+      alignment: 'right',
+      floatImage: {
+        url: '/image.jpg'
+      }
+    }
+
+    expect(highlightMapper(highlight)).toStrictEqual({
+      title: 'title',
+      subtitle: 'subtitle',
+      backgroundImage: 'http://localhost:1337/image.jpg',
+      buttonLabel: 'button label',
+      buttonLink: 'button link',
+      alignment: 'right',
+      floatImage: 'http://localhost:1337/image.jpg'
+    })
+  })
+})
