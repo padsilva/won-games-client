48ee7732844094dd96368726791fce8701eb51c3
paulosilva <paulosilva@chinasystems.com>
2021-03-01 16:40:48 +0000

    remove some files from test coverage

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,10 @@ module.exports = {
     '!src/**/stories.jsx',
     '!src/pages/**/*.jsx',
     '!src/styles/**/*.js',
-    '!src/utils/apollo.js'
+    '!src/utils/apollo.js',
+    '!src/utils/apolloCache.js',
+    '!src/graphql/**/*.js',
+    '!src/**/mock.js'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest/']
