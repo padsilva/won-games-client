46d0c47f7e0cbaf4362e2e7a2bc53a8856a5238b
paulosilva <paulosilva@chinasystems.com>
2021-03-15 11:42:19 +0000

    add auth provider

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
 import Head from 'next/head'
+import { Provider as AuthProvider } from 'next-auth/client'
 import NextNprogress from 'nextjs-progressbar'
 import { ThemeProvider } from 'styled-components'
 import { ApolloProvider } from '@apollo/client'
@@ -13,30 +14,32 @@ const App = ({ Component, pageProps }) => {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-192.png" />
-            <link rek="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="The best game store in the world!"
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch-icon" href="/img/icon-192.png" />
+              <link rek="manifest" href="/manifest.json" />
+              <meta
+                name="description"
+                content="The best game store in the world!"
+              />
+            </Head>
+            <GlobalStyles />
+            <NextNprogress
+              color={theme.colors.primary}
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={5}
             />
-          </Head>
-          <GlobalStyles />
-          <NextNprogress
-            color={theme.colors.primary}
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={5}
-          />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </AuthProvider>
   )
 }
 
---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,49 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+const options = {
+  pages: {
+    signIn: '/sign-in'
+  },
+  providers: [
+    Providers.Credentials({
+      name: 'Sign-in',
+      credentials: {},
+      async authorize({ email, password }) {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/auth/local`,
+          {
+            method: 'POST',
+            body: new URLSearchParams({ identifier: email, password })
+          }
+        )
+
+        const { data } = await response.json()
+
+        return data.user ? { ...data.user, jwt: data.jwt } : null
+      }
+    })
+  ],
+  callbacks: {
+    session: async (session, user) => {
+      session.jwt = user.jwt
+      session.id = user.id
+
+      return Promise.resolve(session)
+    },
+    jwt: async (token, user) => {
+      if (user) {
+        token.id = user.id
+        token.email = user.email
+        token.name = user.username
+        token.jwt = user.jwt
+      }
+
+      return Promise.resolve(token)
+    }
+  }
+}
+
+const Auth = (req, res) => NextAuth(req, res, options)
+
+export default Auth
